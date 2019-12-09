22159a82a76b3f7f0ceeb83f95d7dc79512fbe81
lyb <975965948@qq.com>
2019-12-09 08:24:55 +0800

    安全漏洞

---
+++ src/components/TableRow.vue
@@ -0,0 +1,21 @@
+<template>
+  <tr>
+    <td style="width:100px" v-for="(prop, key) in columns" :key="key">{{item[key]}}</td>
+  </tr>
+</template>
+<script>
+/* eslint no-console:0*/
+export default {
+  name: 'TableRow',
+  props: {
+    item : Object,
+    columns : Object
+  },
+  methods: {
+
+  },
+  destroyed(){
+    console.log(this.item.title)
+  }
+}
+</script>
